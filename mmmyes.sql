3187616d74a94a105c37805883b641c27f418d8e
mbaxter <github@phozop.net>
2011-03-01 12:24:52 -0500

    reports sql
    REPORTS DON'T WORK YET >.>

--- mmmyes.sql
+++ mmmyes.sql
@@ -80,4 +80,21 @@ CREATE TABLE IF NOT EXISTS `warps` (
   KEY `player` (`player`),
   KEY `name` (`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
- 
+
+ CREATE TABLE IF NOT EXISTS `reports` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `user` varchar(32) COLLATE utf8_bin NOT NULL,
+  `message` text COLLATE utf8_bin NOT NULL,
+  `closed` tinyint(1) NOT NULL DEFAULT '0',
+  `admin` varchar(32) COLLATE utf8_bin NOT NULL DEFAULT '""',
+  `reason` text COLLATE utf8_bin NOT NULL,
+  `world` varchar(32) COLLATE utf8_bin NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `z` double NOT NULL,
+  `pitch` float NOT NULL,
+  `yaw` float NOT NULL,
+  `server` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
+
