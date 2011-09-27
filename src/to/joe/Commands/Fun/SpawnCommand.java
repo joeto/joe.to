9d2740ac1d6324d43707c4460fd0f589ae0007f2
Collin Day <collin@collinday.com>
2011-09-26 20:44:47 -0700

    HEEE RANDOM

--- src/to/joe/Commands/Fun/SpawnCommand.java
+++ src/to/joe/Commands/Fun/SpawnCommand.java
@@ -1,6 +1,7 @@
 package to.joe.Commands.Fun;
 
 import java.util.List;
+import java.util.Random;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 import to.joe.J2;
 import to.joe.Commands.MasterCommand;
 import to.joe.util.Flag;
+import to.joe.util.Warp;
 
 public class SpawnCommand extends MasterCommand {
 
@@ -21,12 +23,21 @@ public class SpawnCommand extends MasterCommand {
         if (!isPlayer || this.j2.hasFlag(player, Flag.FUN)) {
             if (isPlayer && (!this.j2.hasFlag(player, Flag.ADMIN) || (args.length < 1))) {
                 player.sendMessage(ChatColor.RED + "WHEEEEEEEEEEEEEEE");
-                this.j2.safePort(player, player.getWorld().getSpawnLocation());
+                Random rand = new Random();
+                int randint = rand.nextInt((12+1) - 1) + 1;
+                String randWarp =  Integer.toString(randint);
+                final Warp warp = this.j2.warps.getPublicWarp(randWarp);
+                this.j2.safePort(player, warp.getLocation());
+
             } else if (args.length == 1) {
                 final List<Player> inquest = this.j2.getServer().matchPlayer(args[0]);
                 if (inquest.size() == 1) {
                     final Player inquestion = inquest.get(0);
-                    this.j2.safePort(inquestion, inquestion.getWorld().getSpawnLocation());
+                    Random rand = new Random();
+                    int randint = rand.nextInt((12+1) - 1) + 1;
+                    String randWarp =  Integer.toString(randint);
+                    final Warp warp = this.j2.warps.getPublicWarp(randWarp);
+                    this.j2.safePort(player, warp.getLocation());
                     inquestion.sendMessage(ChatColor.RED + "OH GOD I'M BEING PULLED TO SPAWN OH GOD");
                     this.j2.sendAdminPlusLog(ChatColor.RED + playerName + " pulled " + inquestion.getName() + " to spawn");
                 } else {
